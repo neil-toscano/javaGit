c8f47406e00e372003bc594b5f5230f23465a414
neil <jacoborosseau@gmail.com>
2021-01-10 07:35:05 -0500

    for y otros

--- Git.java
+++ Git.java
@@ -17,6 +17,9 @@ public class Git {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hol mundo");
+        for(int i=0;i<10;i++){
+            
+        }
     }
     
 }
---
+++ empezandogit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author HP
+ */
+public class empezandogit {
+    
+    public static void main(String[] args){
+        System.out.println("curso de git");
+    }
+}
